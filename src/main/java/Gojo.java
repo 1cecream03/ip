62b8d52f8ed6c384eeaf45776de9ee65c4ebdef2
1cecream03 <105071325+1cecream03@users.noreply.github.com>
2026-01-29 02:18:25 +0800

    Rename Duke to Gojo and update greeting

---
+++ src/main/java/Gojo.java
@@ -0,0 +1,18 @@
+public class Gojo {
+
+    public static void main(String[] args) {
+        String name = "Gojo";
+
+        printLine();
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        printLine();
+
+        System.out.println("Bye. Hope to see you again soon!");
+        printLine();
+    }
+
+    public static void printLine() {
+        System.out.println("____________________________________________________________");
+    }
+}
