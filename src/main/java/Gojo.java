7d0e973557c9157f871a27373183dfd1f0206d65
1cecream03 <105071325+1cecream03@users.noreply.github.com>
2026-01-30 03:55:15 +0800

    Implement Echo feature.

--- src/main/java/Gojo.java
+++ src/main/java/Gojo.java
@@ -1,12 +1,27 @@
+import java.util.Scanner;
+
 public class Gojo {
 
     public static void main(String[] args) {
         String name = "Gojo";
+        Scanner in = new Scanner(System.in);
 
         printLine();
         System.out.println("Throughout heaven and earth, I alone am the honored one.");
         System.out.println("I'm " + name + ". Ask me anything.");
         printLine();
+
+        String command;
+        while (true) {
+            command = in.nextLine();
+            if (command.equals("bye")) {
+                break;
+            }
+
+            printLine();
+            System.out.println(command);
+            printLine();
+        }
 
         System.out.println("See ya!");
         printLine();
